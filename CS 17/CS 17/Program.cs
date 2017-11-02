4fc8db74f8a992cb5f3d53af0c37e1b28e821da2
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:23:06 -0400

    First commit

---
+++ CS 17/CS 17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+               
+                Console.WriteLine("Hello world");
+                throw new Exception("Goodbye cruel world");
+                Console.WriteLine("Goodbye");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: C# 17 - Error Handling
+Published: 9/29/2011 3:38:57 PM
+Link: https://www.youtube.com/watch?v=DzUFm2FNeyo
+
+Description: Learn how to create and handle errors
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
